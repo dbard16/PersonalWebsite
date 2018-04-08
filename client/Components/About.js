3a6103993676607fe85b823fbdd7ae183f4cc108
Dan Weissbard <danweissbard@gmail.com>
2018-04-08 11:34:19 -0400

    fade in animation

--- client/Components/About.js
+++ client/Components/About.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
-
+import Fade from 'react-reveal/Fade'
 export default class About extends Component {
 
-constructor(){
-  super();
-  this.state = {
-    name: ''
-  }
-}
 
-componentDidMount () {
-  console.log('made it');
-
-}
   render () {
     return (
       <div>
-        This is a test
-        <p> a </p>
-}
-
-<p> a </p>
-<p> a </p>
-<p> a </p>
-<p> a </p>
-<p> a </p>
-<p> a </p>
-<p> a </p>
+        <div>
+          <Fade left>
+            <h1> React Reveal </h1>
+          </Fade>
+          <Fade left>
+            <h1> React abc </h1>
+          </Fade>
+          <Fade left>
+            <h1> React def </h1>
+          </Fade>
+          <Fade left>
+            <h1> React ghi </h1>
+          </Fade>
+          <Fade left>
+            <h1> React jkl </h1>
+          </Fade>
+          <Fade left>
+            <h1> React mno </h1>
+          </Fade>
+          <Fade left>
+            <h1> React pqr </h1>
+          </Fade>
+          <Fade left>
+            <h1> React stu </h1>
+          </Fade>
+          <Fade left>
+            <h1> React vwx </h1>
+          </Fade>
+          <Fade left>
+            <h1> React yz </h1>
+          </Fade>
+          <Fade left>
+            <h1> React 123 </h1>
+          </Fade>
+          <Fade left>
+            <h1> React 456 </h1>
+          </Fade>
+        </div>
 
       </div>
     )
