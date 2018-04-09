455e189618e3037713ed83c57eb982713c3bbf16
Dan Weissbard <danweissbard@gmail.com>
2018-04-08 20:22:47 -0400

    css improvements

--- client/Components/Contact.js
+++ client/Components/Contact.js
@@ -4,10 +4,14 @@ export default class Projects extends Component {
 
   render () {
     return (
-      <div>
+      <div className="container-fluid" style={{margin: 0, paddingLeft: 0, paddingRight: 0}}>
+        <div className="content">
+          <h1 className="pageHeader"> Contact </h1>
+
         <button type="button"><a href="https://www.linkedin.com/in/dan-weissbard/" target="_blank"> <img src="./public/linkedin.png" /> </a></button>
         <button type="button"><a href="https://www.github.com/dbard16" target="_blank"> <img src="./public/github.png" /> </a></button>
-        <button type="button"><a href="mailto:danweissbard@gmail.com?Subject=Saw%20Your%20Website" target="_top"> <img src="./public/gmail.png" /> </a></button>
+        <button type="button"><a href="mailto:danweissbard@gmail.com" target="_top"> <img src="./public/gmail.png" /> </a></button>
+      </div>
       </div>
     )
 
--- client/Components/Projects.js
+++ client/Components/Projects.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-
+import Fade from 'react-reveal/Fade'
 export default class Projects extends Component {
 
-componentDidMount () {
-  console.log('made it 2');
 
-}
   render () {
     return (
-      <div>
-        here are projects!
+      <div className="container-fluid" style={{margin: 0, paddingLeft: 0, paddingRight: 0}}>
+        <div className="content">
+          <h1 className="pageHeader"> Projects </h1>
+
+          <Fade bottom>
+            <div>
+              BarCast - https://djrc-api.herokuapp.com/#/
+              Github Repos: https://github.com/cfitzgerald/DJRC-API
+              https://github.com/ravishrawal/DJRC
+
+            </div>
+
+            <div>
+              Grace Shopper
+              https://github.com/drinkingChai/grace-shopper
+            </div>
+
+            <div>
+              Choose Your Own Adventure
+            </div>
+          </Fade>
+
+        </div>
+
       </div>
     )
 
